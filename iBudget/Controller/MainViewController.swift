aa4a29c5c4f55d3078fe44f7260bc1c745fdeff2
martinusgalih <martinusgalih98@gmail.com>
2021-06-17 22:49:13 +0700

    shadow

--- iBudget/Controller/MainViewController.swift
+++ iBudget/Controller/MainViewController.swift
@@ -13,17 +13,22 @@ class MainViewController: UIViewController {
     var countFired: CGFloat = 0
     @IBOutlet weak var progressBar: plainProgressBar!
     @IBOutlet weak var bgView: UIView!
+    @IBOutlet weak var backButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        bgView.layer.cornerRadius = 10
-        bgView.clipsToBounds = true
+       
+// shadow for bgView
+        bgView.layer.cornerRadius = 20
+        bgView.layer.shadowColor = UIColor.black.cgColor
+        bgView.layer.shadowOffset = CGSize(width: 0, height: 1.0)
+        bgView.layer.shadowOpacity = 0.2
+        bgView.layer.shadowRadius = 4.0
+        
         // Do any additional setup after loading the view.
         Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { Timer in
             self.countFired += 1
-            
             DispatchQueue.main.async {
                 self.progressBar.progress = min(0.03 * self.countFired, 1)
-                
                 if self.progressBar.progress == 1 {
                     Timer.invalidate()
             }
