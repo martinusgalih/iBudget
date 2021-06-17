9a8ee8dddc62e9c1c2817b7991011a5169eb800f
martinusgalih <martinusgalih98@gmail.com>
2021-06-18 01:04:33 +0700

    poles2 manja

--- iBudget/Controller/MainViewController.swift
+++ iBudget/Controller/MainViewController.swift
@@ -14,15 +14,26 @@ class MainViewController: UIViewController {
     @IBOutlet weak var progressBar: plainProgressBar!
     @IBOutlet weak var bgView: UIView!
     @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var mainImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
        
+        mainImageView.animationImages = 
+    
+         view.addSubview(mainImageView)
+         mainImageView.startAnimating()
+        mainImageView.animationDuration = 3
+        mainImageView.animationRepeatCount = 1
+        
 // shadow for bgView
         bgView.layer.cornerRadius = 20
         bgView.layer.shadowColor = UIColor.black.cgColor
         bgView.layer.shadowOffset = CGSize(width: 0, height: 1.0)
-        bgView.layer.shadowOpacity = 0.2
+        bgView.layer.shadowOpacity = 0.5
         bgView.layer.shadowRadius = 4.0
+        
+        addButton.layer.cornerRadius = 10
         
         // Do any additional setup after loading the view.
         Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { Timer in
@@ -39,14 +50,12 @@ class MainViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+    
+    
+    
+    
 
 }
+
+
