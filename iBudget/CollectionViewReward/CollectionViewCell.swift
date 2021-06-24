c95b4e98efdb4e4900ff07f776637de64d922241
Julius Eduvin <julius_eduvin@yahoo.com>
2021-06-24 10:41:50 +0700

    terbaru guys

---
+++ iBudget/CollectionViewReward/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  iBudget
+//
+//  Created by Julius Eduvin on 24/06/21.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellImageView: UIImageView!
+    @IBOutlet weak var cellLabelView: UILabel!
+}
--- iBudget/ViewController.swift
+++ iBudget/ViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout  {
+    
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    let reward = [
+        (nama:"Reward Satu", gambar:"reward1"),
+        (nama:"Reward Dua", gambar:"reward2"),
+        (nama:"Reward Tiga", gambar:"reward3"),
+        (nama:"Reward Empat", gambar:"reward4"),
+        (nama:"Reward Lima", gambar:"reward5"),
+        (nama:"Reward Enam", gambar:"reward6")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 5
+        layout.minimumInteritemSpacing = 5
+        collectionView.setCollectionViewLayout(layout, animated: true)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 1.0, left: 1.0, bottom: 1.0, right: 1.0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let lay = collectionViewLayout as! UICollectionViewFlowLayout
+        let widthPerItem = collectionView.frame.width / 2 - lay.minimumInteritemSpacing
+
+        return CGSize(width: widthPerItem, height: 250)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return reward.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
+        let rewards = reward[indexPath.row]
+        
+        cell.cellLabelView.text = rewards.nama
+        cell.cellImageView.image = UIImage(named: rewards.gambar)
+        return cell
     }
 
 
 }
+
 
